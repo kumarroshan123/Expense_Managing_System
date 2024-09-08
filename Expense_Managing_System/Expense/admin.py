c54ac561a153e1f5d8c5b1e8db634037cfea86bf
kumarroshan123 <kumar30roshan@gmail.com>
2024-09-08 22:40:17 +0530

    Update

--- Expense_Managing_System/Expense/admin.py
+++ Expense_Managing_System/Expense/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import BankAccount, Expenses
 # Register your models here.
+
+admin.site.register(BankAccount)
+admin.site.register(Expenses)
--- Expense_Managing_System/Expense_Managing_System/settings.py
+++ Expense_Managing_System/Expense_Managing_System/settings.py
@@ -77,8 +77,8 @@ WSGI_APPLICATION = 'Expense_Managing_System.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'djongo',
+        'NAME': 'expense_mongo',  # Name of your MongoDB database
     }
 }
 
