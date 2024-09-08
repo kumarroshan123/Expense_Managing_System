1019e6f4b79a3d1d9ffde9859ed466bce47fd9b2
kumarroshan123 <kumar30roshan@gmail.com>
2024-09-08 21:08:52 +0530

    Bank_Account_ViewSet

--- Expense_Managing_System/Expense/views.py
+++ Expense_Managing_System/Expense/views.py
@@ -1,9 +1,23 @@
 from django.shortcuts import render
 from rest_framework import viewsets
-from django.contrib.auth.models import User
-from .serializers import UserSerializer
+from django.contrib.auth.models import User 
+from .models import BankAccount
+from .serializers import UserSerializer , BankAccountSerializer , ExpenseSerializer
 
 # Create your views here.
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+class BankAccountViewSet(viewsets.ModelViewSet):
+    serializer_class = BankAccountSerializer
+
+    def get_queryset(self):
+        user_id = self.kwargs['user_id']
+        return BankAccount.objects.filter(user_id=user_id)
+
+    
+
+    
+
+    
