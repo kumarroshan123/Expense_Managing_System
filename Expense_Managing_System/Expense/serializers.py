0203777ed95bb48ea2aac92fbf22fe8fe49a7228
kumarroshan123 <kumar30roshan@gmail.com>
2024-09-08 20:08:19 +0530

    Bank_Account_Serializer

--- Expense_Managing_System/Expense/serializers.py
+++ Expense_Managing_System/Expense/serializers.py
@@ -1,8 +1,17 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
+from .models import BankAccount
 
 class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
         fields = ['username', 'email']
+
+
+class BankAccountSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = BankAccount
+        fields = ['user', 'account_number', 'account_name', 'balance']
+
+    
