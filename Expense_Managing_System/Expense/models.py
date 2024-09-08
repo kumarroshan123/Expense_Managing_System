12b7b239004a955614d8eb3f43a53d2f901155d7
kumarroshan123 <kumar30roshan@gmail.com>
2024-09-08 19:35:31 +0530

    Bank_Account_Model

--- Expense_Managing_System/Expense/models.py
+++ Expense_Managing_System/Expense/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class BankAccount(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    account_name = models.CharField(max_length=100)
+    account_number = models.CharField(max_length=20, unique=True)
+    balance = models.DecimalField(max_digits=10,decimal_places=2)
+
+    def __str__(self):
+        return f"{self.account_name} ({self.account_number})"
